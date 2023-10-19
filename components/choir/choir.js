3df2c37bf8805a2009a2945cbd57fbbf4cac20bd
Kayphrosh <olakunbiolabode@yahoo.com>
2023-10-19 17:47:16 +0100

    fixing styling

--- components/choir/choir.js
+++ components/choir/choir.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import bg from '../../public/Images/choir.jpg'
+
 const Choir = () => {
   return (
     <div className='choir-container' style={{backgroundImage: `url(${bg.src})`,}}>
       <div className='choir-text'>
         <p>KSF Choir</p>
-        <h3>Meet Kingdom Student Fellowship Chiour During Our Christmas Ceremony</h3>
+        <h3>Meet Kingdom Life Student Fellowship Chior During Our Christmas Ceremony</h3>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Heroe from '@/components/heroe/heroe'
+import Heroe from '@/components/hero/hero'
 import About from '@/components/about/about'
 import Founders from '@/components/founnders/founders'
 import Event from '@/components/events/event'
 import Footer from '@/components/footer/footer'
 import Choir from '@/components/choir/choir'
+import President from '@/components/president/president '
 import { amp } from '@/next.config'
 
 export default function Home() {
@@ -13,7 +14,9 @@ export default function Home() {
     <>
       <Heroe />
       <About/>
+      <President />
       <Choir />
+
       <Founders />
       <Event />
       <Footer />
